c127b3f3995228df5295dc1a561debf5b2a0130d
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-04-09 17:09:44 +0700

    feat: implement SPA hash-based router and chatbot API engine with metadata bootstrap

--- sql/Bootstrap_API_Metadata_Auto.sql
+++ sql/Bootstrap_API_Metadata_Auto.sql
@@ -192,43 +192,34 @@ BEGIN
     SET NOCOUNT ON;
 
     DECLARE @ActualSPName VARCHAR(200);
+    DECLARE @ApiID INT;
 
     -- 1. Tìm chính quy trên API_Definition
-    SELECT TOP 1 @ActualSPName = StoredProcedure
+    SELECT TOP 1 @ActualSPName = StoredProcedure, @ApiID = ApiID
     FROM dbo.API_Definition
     WHERE ApiCode = @ApiCode OR ApiCode = '@' + REPLACE(@ApiCode, '@', '');
-
-    -- 2. Nếu rỗng, ép bỏ gạch dưới "_" để tìm Mò cực mạnh trên Base SQL
-    IF @ActualSPName IS NULL
-    BEGIN
-        DECLARE @CleanCode VARCHAR(100) = REPLACE(REPLACE(@ApiCode, '@', ''), '_', '');
-
-        SELECT TOP 1 @ActualSPName = name 
-        FROM sys.procedures 
-        WHERE REPLACE(name, '_', '') LIKE '%' + @CleanCode + '%' 
-          AND name LIKE '%_AI';
-    END
 
     -- Nếu tìm không ra SP nào
     IF @ActualSPName IS NULL
     BEGIN
-        SELECT FieldCode = '@Invalid', FieldName = N'Bó tay! Không tìm thấy SP nào tương tự: ' + @ApiCode, IsRequired = 0, Placeholder = N'';
+        SELECT FieldCode = '@Invalid', FieldName = N'Bó tay! Không tìm thấy SP: ' + @ApiCode, IsRequired = 0, Placeholder = N'';
         RETURN;
     END
 
-    -- 3. In ra Config tham số
+    -- Lấy thông tin cấu hình CHUẨN từ bảng API_Field (để ăn các cài đặt Override, Default, ControlType)
     SELECT 
-        FieldCode   = prm.name, 
-        FieldName   = REPLACE(prm.name, '@', ''),
-        Placeholder = N'Nhập ' + REPLACE(prm.name, '@', '') + ' (' + UPPER(t.name) + ')',
-        IsRequired  = CASE WHEN prm.has_default_value = 1 THEN 0 ELSE 1 END,
-        ControlType = 'TEXT',
-        OrderIndex  = prm.parameter_id
-    FROM sys.procedures p
-    JOIN sys.parameters prm ON prm.object_id = p.object_id
-    JOIN sys.types t ON t.user_type_id = prm.user_type_id
-    WHERE p.name = @ActualSPName
-    ORDER BY prm.parameter_id;
+        FieldCode   = f.FieldCode, 
+        FieldName   = REPLACE(f.FieldCode, '@', ''),
+        Placeholder = COALESCE(f.Placeholder, N'Nhập ' + REPLACE(f.FieldCode, '@', '')),
+        IsRequired  = ISNULL(f.IsRequired, 0),
+        ControlType = ISNULL(f.ControlType, 'TEXT'),
+        OrderIndex  = f.OrderIndex,
+        IsSystemParam = ISNULL(f.IsSystemParam, 0),
+        DataSourceType = f.DataSourceType,
+        DataSourceValue = f.DataSourceValue
+    FROM dbo.API_Field f
+    WHERE f.ApiID = @ApiID
+    ORDER BY f.OrderIndex;
 END
 GO
 
