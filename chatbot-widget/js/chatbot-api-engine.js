20df9547ddbef2834dcc082e10f19685a8f1f26f
HoangDang <dangnh3549@ut.edu.vn>
2026-05-18 08:43:08 +0700

    Fix UI issue: empty white box overlay when typing autocomplete tokens (@khohang, @sanpham) due to missing Vietnamese column mappings in pickRaw

--- chatbot-widget/js/chatbot-api-engine.js
+++ chatbot-widget/js/chatbot-api-engine.js
@@ -1031,9 +1031,9 @@
 
 
 
-            var dispName = pick(['Name', 'ObjectName', 'FullName', 'label']) || r.label || r.value || '';
+            var dispName = pick(['Name', 'ObjectName', 'FullName', 'label', 'ItemName', 'Sản Phẩm', 'Sản phẩm', 'San Pham', 'San pham', 'TenCuaHang', 'Tên cửa hàng']) || r.label || r.value || '';
 
-            var dispId = pick(['MaDanhMuc', 'MaKhachHang', 'MA_KH', 'ID', 'Code', 'CustomerID']) || r.value || '';
+            var dispId = pick(['MaDanhMuc', 'MaKhachHang', 'MA_KH', 'ID', 'Code', 'CustomerID', 'ItemID', 'DocumentID', 'Mã sp', 'Mã SP', 'Ma sp', 'Ma SP']) || r.value || '';
 
             var pl = pick(['PhanLoai', 'type', 'Type']) || '';
 
@@ -1211,9 +1211,9 @@
 
 
 
-                var mAD = pickRaw(['MaDanhMuc', 'ObjectID', 'MaKhachHang', 'MA_KH', 'CUSTOMER_ID', 'CustomerCode', 'CustomerID', 'Ma', 'Code', 'ID']) || r.value || '';
+                var mAD = pickRaw(['MaDanhMuc', 'ObjectID', 'MaKhachHang', 'MA_KH', 'CUSTOMER_ID', 'CustomerCode', 'CustomerID', 'Ma', 'Code', 'ID', 'ItemID', 'DocumentID', 'Mã sp', 'Mã SP', 'Ma sp', 'Ma SP']) || r.value || '';
 
-                var mName = pickRaw(['Name', 'ObjectName', 'FullName', 'HoTen', 'HOTEN', 'TEN_KH', 'TenKhachHang', 'Ten', 'label']) || r.label || r.Name || '';
+                var mName = pickRaw(['Name', 'ObjectName', 'FullName', 'HoTen', 'HOTEN', 'TEN_KH', 'TenKhachHang', 'Ten', 'label', 'ItemName', 'Sản Phẩm', 'Sản phẩm', 'San Pham', 'San pham', 'TenCuaHang', 'Tên cửa hàng']) || r.label || r.Name || '';
 
                 var mPL = pickRaw(['PhanLoai', 'type', 'Type']) || '';
 
